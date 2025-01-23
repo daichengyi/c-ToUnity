41b8de068a009722e2e8fe8d5304e00ee0a92b8c
daichengyi <443859613@qq.com>
2025-01-23 15:40:04 +0800

    switch

---
+++ C#初级课程之语法基础/09switch/switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (false)
+            {
+
+            }else if (true)
+            {
+
+            }
+
+            string str = "isBool";
+            switch (str)
+            {
+                case "true":
+
+                    break;
+                case "isBool":
+
+                    break;
+                    default:
+
+                    break;
+            }
+        }
+    }
+}
