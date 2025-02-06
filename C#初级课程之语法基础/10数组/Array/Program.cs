a55f6730769cda0036b072e4d58e7e73690ec71f
daichengyi <443859613@qq.com>
2025-02-06 17:05:40 +0800

    数组、函数

---
+++ C#初级课程之语法基础/10数组/Array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /** 数组认知*/
+            int[] intArray;
+
+            intArray = new int[5];
+
+            intArray[0] = 1;
+
+            //声明、初始化、赋值
+            double[] doubleArray = new double[] {12,13,14,15,16};
+
+            /** 循环遍历数组*/
+            int[] forArr = new int[5] { 1,2,3,4,5};
+            for (int i = 0; i < forArr.Length; i++)
+            {
+                Console.WriteLine(forArr[i]);
+            }
+
+
+            foreach (var item in doubleArray)
+            {
+                
+            }
+
+
+
+            /** 数组内存结构*/
+            int[] intA = new int[] { 11, 22, 33, 44, 55 };
+            int[] intB = intA;
+            intB[0] = 1;
+
+
+            /** 二维数组*/
+            int[,] intArr2 = new int[2,4] {{11,12,13,14},{21,22,23,24 } };
+            int leng = intArr2.Length;//总长度
+            int len1 = intArr2.GetLength(0);//第一个长度
+            int len2 = intArr2.GetLength(1);//第二个长度
+
+            Console.WriteLine("leng==={0}",leng);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#初级课程之语法基础/11函数/function/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //函数
+            Hello();
+
+            //ref
+            int num = 10;
+            AddRef(ref num);//引用传递
+            Console.WriteLine("AddRef====={0}", num);
+
+            //out
+            int a = 10;
+            int b = 15;
+            int max;
+            int min;
+            int total;
+            double avg;
+            NumberOut(a, b,out max, out min, out total, out avg);
+
+
+            //递归
+            Console.WriteLine(NumberAdd(100));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        static string Hello()
+        {
+            Console.WriteLine("函数被调用了！");
+
+            return "返回值";
+        }
+
+
+        /** 函数重载*/
+        static string Hello(string str)
+        {
+            return str;
+        }
+
+        static string Hello(int str)
+        {
+            return str.ToString();
+        }
+
+
+        /** 函数之高级参数*/
+        static void Add(int num)
+        {
+            Console.WriteLine("start====={0}",num);
+            num += 10;
+            Console.WriteLine("end====={0}", num);
+        }
+
+        //ref 参数
+        static void AddRef(ref int num)
+        {
+            num += 10;
+            Console.WriteLine("void AddRef====={0}", num);
+        }
+
+
+        //out，返回 最大、最小、和、平均值
+        static void NumberOut( int a,int b, out int max, out int min,out int total,out double avg)
+        {
+            if(a > b)
+            {
+                max = a; 
+                min = b;
+            }
+            else
+            {
+                max = b; 
+                min = a;
+            }
+            total = a + b;
+            avg = total / 2;
+        }
+
+        /** 函数递归调用*/
+        //实现1+2+3+.....+N之和
+        static int NumberAdd(int n)
+        {
+            if(n == 1)  return 1;
+            return n + NumberAdd(n-1);
+        }
+    }
+}
